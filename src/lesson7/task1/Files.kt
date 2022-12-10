007a4d9e9ccad075d1fb9128bbb855d3799d1a0e
elvinashukurova <shukurova2004@inbox.ru>
2022-12-10 23:54:50 +0300

    done

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -144,10 +144,10 @@ fun centerFile(inputName: String, outputName: String) {
     var c = 0
     var k = ""
     for (i in list) {
+        val i = i.trim()
         if (i == "") {
             writer.write(i)
         }
-        val i = i.trim()
         c = (d - i.length) / 2
         k = " ".repeat(c) + i
         writer.write(k)
